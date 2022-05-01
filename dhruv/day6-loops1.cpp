e18be80e0ec35d276fb9cfe3693074eca370576f
dhruv059 <99869433+dhruv059@users.noreply.github.com>
2022-05-01 21:37:05 +0530

    Create day6-loops1.cpp

---
+++ dhruv/day6-loops1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(0);
+{
+int n;
+int sum=0;
+cin>>n;
+for (int i = 0; i <=n; i++)
+{
+     sum+=i;
+
+}
+cout<<sum;
+
+	
+
+return 0;
+}
+}
